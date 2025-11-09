4ae97c7cca6ce099ee9d70436670501821fe86d2
git.sujalsingh@gmail.com <git.sujalsingh@gmail.com>
2025-11-09 13:45:55 +0530

    comments updated/stored locally

--- style.js
+++ style.js
@@ -1,3 +1,4 @@
+// sets theme if dark turns light or light turns dark; 
 function settheme(){
 if(window.matchMedia("(prefers-color-scheme: dark)").matches){
     document.body.classList.add("dark");
@@ -7,22 +8,28 @@ if(window.matchMedia("(prefers-color-scheme: dark)").matches){
   document.body.classList.remove("dark"); 
 }
 }
+
+// changes theme on click; 
 let btn = document.querySelector("button");
 btn.addEventListener("click", function(){
   if(document.body.classList.contains("dark")){
     document.body.classList.remove("dark");
     document.body.classList.add("light");
+
+    localStorage.setItem("theme","light");
   } 
   else{
     document.body.classList.remove("light");
     document.body.classList.add("dark");
+
+    localStorage.setItem("theme","dark");
   }
 
 })
 
-
+// add event according to os; 
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",function(){
   settheme();
 });
-
+// set theme on reload; 
 settheme();
